bee9fe55e8d46ed46c4e2ee6e6cbe0257b1fbead
Akshay Gaikwad <101447868+argaikwad96@users.noreply.github.com>
2025-11-27 16:01:28 +0530

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from string_utils import reverse_string, to_upper, to_lower, count_vowels
+
+if __name__ == "__main__":
+    sample ="Hello world"
+
+
+    print("Original:",sample)
+    print("Reversed:", reverse_string(sample))
+    print("Uppercase:", to_upper(sample))
+    print("Lowercase:", to_lower(sample))
+    print("Vowel Count:", count_vowels(sample))
